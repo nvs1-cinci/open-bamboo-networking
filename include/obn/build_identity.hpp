dd8ab161ee16762d3dd9696766c06bd0fc6494e0
Alexey Cluster <cluster@cluster.wtf>
2026-06-15 17:55:52 +0200

    CI/CD fix
    Signed-off-by: Alexey Cluster <cluster@cluster.wtf>

--- include/obn/build_identity.hpp
+++ include/obn/build_identity.hpp
@@ -6,22 +6,43 @@
 // OBN_GIT_COMMIT / OBN_GIT_DIRTY — interim build identity when git is available.
 
 #ifdef OBN_RELEASE_BUILD
-#  define OBN_PLUGIN_LOAD_BANNER_MSG \
-       "Loaded Open Bamboo Networking plugin, version " OBN_PROJECT_VERSION_STRING \
-       ", ABI " OBN_VERSION_STRING
+#  ifdef OBN_VERSION_STRING
+#    define OBN_PLUGIN_LOAD_BANNER_MSG \
+         "Loaded Open Bamboo Networking plugin, version " OBN_PROJECT_VERSION_STRING \
+         ", ABI " OBN_VERSION_STRING
+#  else
+#    define OBN_PLUGIN_LOAD_BANNER_MSG \
+         "Loaded Open Bamboo Networking plugin, version " OBN_PROJECT_VERSION_STRING
+#  endif
 #else
 #  ifdef OBN_GIT_COMMIT
 #    ifdef OBN_GIT_DIRTY
-#      define OBN_PLUGIN_LOAD_BANNER_MSG \
-           "Loaded Open Bamboo Networking plugin, interim build, commit #" OBN_GIT_COMMIT \
-           " (dirty), ABI " OBN_VERSION_STRING
+#      ifdef OBN_VERSION_STRING
+#        define OBN_PLUGIN_LOAD_BANNER_MSG \
+             "Loaded Open Bamboo Networking plugin, interim build, commit #" OBN_GIT_COMMIT \
+             " (dirty), ABI " OBN_VERSION_STRING
+#      else
+#        define OBN_PLUGIN_LOAD_BANNER_MSG \
+             "Loaded Open Bamboo Networking plugin, interim build, commit #" OBN_GIT_COMMIT \
+             " (dirty)"
+#      endif
 #    else
-#      define OBN_PLUGIN_LOAD_BANNER_MSG \
-           "Loaded Open Bamboo Networking plugin, interim build, commit #" OBN_GIT_COMMIT \
-           ", ABI " OBN_VERSION_STRING
+#      ifdef OBN_VERSION_STRING
+#        define OBN_PLUGIN_LOAD_BANNER_MSG \
+             "Loaded Open Bamboo Networking plugin, interim build, commit #" OBN_GIT_COMMIT \
+             ", ABI " OBN_VERSION_STRING
+#      else
+#        define OBN_PLUGIN_LOAD_BANNER_MSG \
+             "Loaded Open Bamboo Networking plugin, interim build, commit #" OBN_GIT_COMMIT
+#      endif
 #    endif
 #  else
-#    define OBN_PLUGIN_LOAD_BANNER_MSG \
-         "Loaded Open Bamboo Networking plugin, interim build, ABI " OBN_VERSION_STRING
+#    ifdef OBN_VERSION_STRING
+#      define OBN_PLUGIN_LOAD_BANNER_MSG \
+           "Loaded Open Bamboo Networking plugin, interim build, ABI " OBN_VERSION_STRING
+#    else
+#      define OBN_PLUGIN_LOAD_BANNER_MSG \
+           "Loaded Open Bamboo Networking plugin, interim build"
+#    endif
 #  endif
 #endif
